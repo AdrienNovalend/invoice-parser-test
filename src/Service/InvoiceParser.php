065fdc8ee23c7b7fc15c102dd1b94ffb1e7ad1ad
Adriii <adrien@novalend.com>
2025-02-04 17:33:45 +0100

    🔥

--- src/Service/InvoiceParser.php
+++ src/Service/InvoiceParser.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 namespace App\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 
 
 class InvoiceParser
@@ -17,24 +18,58 @@ class InvoiceParser
         $this->em = $em;
     }
 
+    /**
+     * @throws Exception
+     */
     public function parse(string $filePath): void
     {
-        if (str_contains($filePath, 'json')) {
-            $data = json_decode(file_get_contents($filePath), true);
-            foreach ($data as $row) {
-                $this->em->getConnection()->executeStatement(
-                    "UPDATE invoice SET amount = {$row['montant']} WHERE name = '{$row['nom']}'"
-                );
-            }
-        } elseif (str_contains($filePath, 'csv')) {
-            $data = array_map(function($row) {
-                return str_getcsv($row, "\t");
-            }, file($filePath));
-            foreach ($data as $row) {
-                $this->em->getConnection()->executeStatement(
-                    "UPDATE invoice SET amount = {$row[0]} WHERE name = '{$row[2]}'"
-                );
-            }
+        //On peux ajouter des logs mais je vais pas y passer 1h non plus ¯\_(ツ)_/¯
+        switch (true) {
+            case str_contains($filePath, 'json'):
+                $content = file_get_contents($filePath);
+
+                if ($content === false) {
+                    throw new Exception(sprintf("Could not read json file with path: %s", $filePath));
+                }
+                $data = json_decode($content , true);
+                foreach ($data as $row) {
+                    $query = $this->em->createQueryBuilder()
+                        ->update('App\Entity\Invoice', 'i')
+                        ->set('i.amount', ':amount')
+                        ->where('i.name = :name')
+                        ->getQuery();
+
+                    $query->execute([
+                        'amount' => $row['montant'],
+                        'name' => $row['nom']
+                    ]);
+
+                }
+                break;
+            case str_contains($filePath, 'csv'):
+                $content = file($filePath);
+
+                if ($content === false) {
+                    throw new Exception(sprintf("Could not read csv file with path: %s", $filePath));
+                }
+                $data = array_map(function($row) {
+                    return str_getcsv($row, "\t");
+                },$content);
+
+                foreach ($data as $row) {
+                    $query = $this->em->createQueryBuilder()
+                        ->update('App\Entity\Invoice', 'i')
+                        ->set('i.amount', ':amount')
+                        ->where('i.name = :name')
+                        ->getQuery();
+
+                    $query->execute([
+                        'amount' => $row[0],
+                        'name' => $row[2]
+                    ]);
+
+                }
+                break;
         }
     }
 }
